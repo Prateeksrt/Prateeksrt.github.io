3cadb92ad1deb163f1cf89f34b73bdde5707104b
Prateek Srivastava <prateeksrt2009@gmail.com>
2020-06-04 22:40:51 +0700

    Initial commit

---
+++ src/components/HelloComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const HelloComponent = () => (<div>Hello World</div>);
+
+export default HelloComponent;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import HelloComponent from './components/HelloComponent';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const wrapper = document.getElementById("content");
+ReactDOM.render(<HelloComponent />, wrapper);
