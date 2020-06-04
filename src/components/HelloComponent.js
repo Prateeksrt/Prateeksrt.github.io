d181cf26dab4c517e4028fd4b0d8a9ad82a53672
Prateek Srivastava <prateeksrt2009@gmail.com>
2020-06-04 23:10:42 +0700

    Removed stuff

--- src/components/HelloComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const HelloComponent = () => (<div>Hello World</div>);
-
-export default HelloComponent;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import HelloComponent from './components/HelloComponent';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const wrapper = document.getElementById("content");
-ReactDOM.render(<HelloComponent />, wrapper);
