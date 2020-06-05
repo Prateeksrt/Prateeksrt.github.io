f4cd953ce0d13d8826f6985d384e57ab51390f19
Prateek Srivastava <prateeksrt2009@gmail.com>
2020-06-05 18:51:22 +0700

    Add css and style loader, add background to body

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import HelloComponent from './components/HelloComponent';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
 const wrapper = document.getElementById("content");
 ReactDOM.render(<HelloComponent />, wrapper);
